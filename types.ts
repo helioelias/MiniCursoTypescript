fe459eed5415a198527248840254f1c32eb9bd5d
Helio Elias de Moura Carvalho <helio.carvalho@unitelco.com.br>
2020-06-15 14:27:52 -0300

    Types

---
+++ types.ts
@@ -0,0 +1,52 @@
+//boolean (ture/false)
+let isOpen: boolean;
+isOpen = true;
+
+//string ('foo', "foo", `foo`)
+let message: string;
+message = 'foo';
+
+//number (int, float, hex, binary)
+let total: number;
+total = 20;
+
+//array (type[])
+let items: number[];
+items = [1,2,3];
+
+let totals: Array<number>;
+totals = [1,2,3];
+
+//tuple
+let title: [number, string, string];
+title = [1, 'foo', "bar"];
+
+//enum
+enum Colors {
+    white = '#fff',
+    black = '#000'
+}
+
+//any (qualquer coisa)
+let coisa: any;
+coisa = "foo";
+
+//void
+
+function logger():void {
+    console.log('');
+}
+
+//null | undefined
+type Bla = string | undefined;
+
+//never
+function error(): never {
+    throw new Error("erro");
+}
+
+//object
+let cart: object;
+cart = {
+    key: "fi"
+};
